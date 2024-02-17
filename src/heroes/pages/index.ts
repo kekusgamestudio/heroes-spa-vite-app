401dde39740a8196886d45d8a2e4d8cb3b1ce799
Sergio <kekuseg@gmail.com>
2024-02-16 22:45:54 -0300

    Multiple routers added

--- src/heroes/pages/index.ts
+++ src/heroes/pages/index.ts
@@ -1,3 +1,5 @@
 export { DcPage } from './DcPage';
 export { MarvelPage } from './MarvelPage';
+export { SearchPage } from './SearchPage';
+export { HeroPage } from './HeroPage';
 
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,17 +1,13 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { DcPage, MarvelPage } from '../heroes/pages';
+import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth/pages/LoginPage';
-import { Navbar } from '../ui/components/';
+import { HeroesRoutes } from '../heroes/routes/HeroesRoutes';
 
 export const AppRouter = () => {
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route path='marvel' element={<MarvelPage />} />
-        <Route path='dc' element={<DcPage />} />
         <Route path='login' element={<LoginPage />} />
-        <Route path='/' element={<Navigate to='/marvel' />} />
+        <Route path='/*' element={<HeroesRoutes />} />
       </Routes>
     </>
   )
