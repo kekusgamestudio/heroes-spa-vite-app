96f19903d98807b87187d941cfcc7a4faae23650
Sergio Martino <sergio.martino@modo.com.ar>
2024-02-18 17:07:54 -0300

    Search form part 1

--- src/heroes/pages/SearchPage.tsx
+++ src/heroes/pages/SearchPage.tsx
@@ -1,6 +1,26 @@
 
 export const SearchPage = () => {
   return (
-    <h1>Search Page</h1>
+    <>
+      <h1>Search Page</h1>
+      <hr />
+
+      <div className='col-5'>
+        <h4>Searching...</h4>
+        <hr />
+        <form action=''>
+          <input 
+            type='text' 
+            placeholder='Search a hero'
+            className='form-control'
+            name='searchTexr'
+            autoComplete='off'
+          />
+          <button className='btn btn-outline-primary mt-1'>
+            Search
+          </button>
+        </form>
+      </div>    
+    </>
   )
 }
